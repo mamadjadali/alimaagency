94376cd8441dc8cd393cf944a368a06687b4cfe0
Mamad Jadali <jadalimamad@gmail.com>
2024-06-05 03:28:39 +0330

    Unleash SCroll

---
+++ src/components/UnleashScroll.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { ContainerScroll } from "./ui/container-scroll-animation";
+
+export function HeroScrollDemo() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-[#0c1e1e]">
+              Unleash the power of <br />
+              <span className="text-4xl md:text-[6rem] text-[#70fe90] font-bold mt-1 leading-none">
+                Digital Presence
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <img
+          src={`/linear.webp`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
