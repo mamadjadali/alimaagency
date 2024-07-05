c8d1045501d98937233e2161b29c365928a34a94
Mamad Jadali <jadalimamad@gmail.com>
2024-07-06 02:56:38 +0330

    color effect on el

--- src/components/ui/NeubrutalismButton.tsx
+++ src/components/ui/NeubrutalismButton.tsx
@@ -7,7 +7,7 @@ interface NeubrutalismButtonProps {
 const NeubrutalismButton: React.FC<NeubrutalismButtonProps> = ({ label }) => {
   return (
     <button className="group/button rounded-lg bg-[#f3f5f4] text-[#0c1e1e]">
-      <span className="block -translate-x-1 -translate-y-1 rounded-lg border-2 border-[#0c1e1e] bg-[#70fe90] px-4 py-1 text-lg font-bold tracking-tight transition-all group-hover/button:-translate-y-2 group-active/button:translate-x-0 group-active/button:translate-y-0">
+      <span className="block -translate-x-1 -translate-y-1 rounded-lg border-2 border-[#0c1e1e] bg-[#70fe90] px-4 py-1 text-lg font-bold tracking-tight transition-all group-hover/button:-translate-x-2 group-hover/button:-translate-y-2 group-active/button:translate-x-0 group-active/button:translate-y-0">
         {label}
       </span>
     </button>
