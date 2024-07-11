6dc074322f66488334d80a02e60507cc1d49fd3d
Mamad Jadali <jadalimamad@gmail.com>
2024-07-11 13:36:41 +0330

    bento web

--- src/components/ui/design/BentoWeb.tsx
+++ src/components/ui/design/BentoWeb.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const BentoWeb = () => {
   return (
-    <div className='w-full h-auto m-5'>
+    <div className='h-auto m-5'>
         <div className='grid grid-cols-2 gap-10'>
             <div className='bg-gradient-to-tr p-6 from-[#70fe90] via-[#19191c] rounded-2xl border border-[#70fe90]'>
                 <h2 className="text-4xl mb-5 font-normal text-[#70fe90]">the way you want</h2>
@@ -26,7 +25,35 @@ const BentoWeb = () => {
                     }}
                 />
             </div>
-            
+        </div>
+        <div className='grid grid-cols-3 gap-10 mt-[2.5rem]'>
+            <div className='bg-transparent p-6 rounded-2xl border border-[#70fe90]'>
+                <h2 className="text-4xl mb-5 font-normal text-[#70fe90]">We Scale for you</h2>
+                <p className="text-[#e5eae6] mb-5">We support many language and frameworks making it flexible to your needs.</p>
+                <img 
+                    src='/scale.png' 
+                    className='right-10'
+                    style={{
+                        maskImage: `linear-gradient(to left, transparent, black 20%)`,
+                    }}
+                />
+            </div>
+            <div className='p-6 rounded-2xl border border-[#70fe90]'>
+                <h2 className="text-4xl mb-5 font-normal text-[#70fe90]">Integrigate with every thing</h2>
+                <p className="text-[#e5eae6] mb-5">We support many language and frameworks making it flexible to your needs.</p>
+                <img 
+                    src='/integ.svg' 
+                    className='mx-auto'
+                />
+            </div>
+            <div className='p-6 rounded-2xl border border-[#70fe90] items-center justify-center'>
+                <h2 className="text-4xl mb-5 font-normal text-[#70fe90]">You know every step</h2>
+                <p className="text-[#e5eae6] mb-5">We support many language and frameworks making it flexible to your needs.</p>
+                <img 
+                    src='/cms.svg'
+                    className='mx-auto'
+                />
+            </div>
         </div>
     </div>
   )
