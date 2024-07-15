58696c163e761da939153538efdc92617518c038
Mamad Jadali <jadalimamad@gmail.com>
2024-07-15 05:22:12 +0330

    Marketing Hero

---
+++ src/components/ui/design/marketing/Hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+    <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 pointer-events-none" aria-hidden="true">
+      <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#dedbcc] to-[#70fe90] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}}></div>
+    </div>
+    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-[#f3f5f4] sm:text-6xl">Elevate Your Brand to New Heights</h1>
+        <p className="mt-6 text-lg leading-8 text-[#e5eae6]">Unlock the true potential of your brand with our comprehensive marketing and advertising solutions.</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a href="#" className="rounded-md bg-[#70fe90] px-3.5 py-2.5 text-sm font-semibold text-balck shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">Get started</a>
+          <a href="#" className="text-sm font-semibold leading-6 text-[#dedbcc]">Learn more <span aria-hidden="true">→</span></a>
+        </div>
+      </div>
+    </div>
+    <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+      <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#70fe90] to-[#dedbcc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}}></div>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
