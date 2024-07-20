83105b3e2d0de6daa05572c8680546a1a77de6a3
Mamad Jadali <jadalimamad@gmail.com>
2024-07-21 01:24:04 +0330

    fix tailwind type err

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+declare module 'preline/plugin' {
+    const plugin: any; // You can replace 'any' with a more specific type if you know it
+    export default plugin;
+}
+
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+    function flattenColorPalette(colors: any): any;
+    export default flattenColorPalette;
+}
+  
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,3 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -31,12 +29,13 @@ export default {
 		},
 	},
 	plugins: [
+		// @ts-ignore
 		require('preline/plugin'),
 		addVariablesForColors,
 	],
 };
 
-function addVariablesForColors({ addBase, theme }: any) {
+function addVariablesForColors({ addBase, theme }) {
 	let allColors = flattenColorPalette(theme("colors"));
 	let newVars = Object.fromEntries(
 	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
