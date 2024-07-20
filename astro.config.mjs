3875066870c2e6aa9ddb48fde47a7fc5b5c4b922
Mamad Jadali <jadalimamad@gmail.com>
2024-07-21 01:14:25 +0330

    remove i18

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mamadjadali.github.io/",
 	base: "/alimaagency",
-  integrations: [react(), tailwind(), astroI18next()]
+  integrations: [react(), tailwind()]
 });
