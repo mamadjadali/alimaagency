93b08ba5d26a23755d20874c15d8cb2abd8b3642
Mamad Jadali <jadalimamad@gmail.com>
2024-07-21 00:45:46 +0330

    fix A bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,13 @@ import { AnimatedModalDemo } from './ui/Modal';
 const Hero = ({title}) => {
   return (
     <>
-    <Aword className='absolute max-w-xs sm:max-w-7xl inset-0 z-0 m-auto blur-md'/>
+    {/* <Aword className='absolute max-w-xs sm:max-w-7xl inset-0 z-0 m-auto blur-md'/> */}
+    <img
+    src='/Alima.png'
+    width={700}
+    height={700}
+    className='absolute max-w-xs sm:max-w-7xl inset-0 z-0 m-auto'
+    />
     <div className="relative isolate px-6 pt-32 md:pt-24 lg:px-8">
         <div className="relative z-10 mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
