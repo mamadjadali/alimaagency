d349dc4fd4bc52fa9dd558d566a5ce834f9b78cc
Mamad Jadali <jadalimamad@gmail.com>
2024-08-10 20:35:49 +0330

    Hero Padding Fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = ({title}) => {
     height={700}
     className='absolute sm:max-w-7xl inset-0 z-0 m-auto'
     />
-    <div className="relative isolate px-6 pt-32 md:pt-24 lg:px-8">
-        <div className="relative z-10 mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+    <div className="relative isolate px-6 pt-28 md:pt-24 lg:px-8">
+        <div className="relative z-10 mx-auto max-w-2xl py-32 sm:py-48 lg:py-36">
           <div className="text-center">
             <h1
                 className="text-[3rem] md:text-[4rem] font-bold text-white"
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -109,7 +109,7 @@ export function AnimatedModalDemo() {
               Cancel
             </button> */}
             <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
-              Next
+              Coming Soon!
             </button>
           </ModalFooter>
         </ModalBody>
