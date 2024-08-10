a69ac757272e67fff5d144cdc13183cea1c39414
Mamad Jadali <jadalimamad@gmail.com>
2024-08-10 21:27:59 +0330

    Google Analytic Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alima.agency",
 	base: "/",
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
