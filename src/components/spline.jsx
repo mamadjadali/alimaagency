7bdcedb8e9c9fa1a78b9401077838687a9ce3561
Mamad Jadali <jadalimamad@gmail.com>
2024-08-23 18:23:50 +0330

    spline test

---
+++ src/components/spline.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Rspline() {
+  return (
+    <Spline scene="https://prod.spline.design/alhn9jdGjkcdrXgH/scene.splinecode" />
+  );
+}
